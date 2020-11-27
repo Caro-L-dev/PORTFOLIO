7909dcd58a4664da28bdb700030265a872d5b5ae
Caro-L-dev <liardcar@gmail.com>
2020-11-27 16:20:03 +0100

    :art: layout welcome page

--- src/components/02_Welcome/index.js
+++ src/components/02_Welcome/index.js
@@ -4,58 +4,59 @@ import { useIntersection } from 'react-use';
 // == Import
 import './welcome.scss';
 import gsap from 'gsap';
+import { element } from 'prop-types';
 
 // == Composant
 function Welcome() {
+  const sectionRef = useRef(null);
+
+  const intersection = useIntersection(sectionRef, {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.5,
+  });
+
+  const fadeIn = (element) => {
+    gsap.to(element, 1, {
+      opacity: 1,
+      y: -60,
+      ease: 'power4.out',
+      stagger: {
+        amount: 0.3,
+      },
+    });
+  };
+
+  const fadeOUt = (element) => {
+    gsap.to(element, 1, {
+      opacity: 0,
+      y: -20,
+      ease: 'power4.out',
+    });
+  };
+
+  intersection && intersection.intersectionRatio < 0.5
+    ? fadeOUt('.fadeIn')
+    : fadeIn('.fadeIn');
+
   return (
     <>
-      <div className="header">
-        <div className="sm-btn">
-          {/* <img src={back} alt="back arrow" /> */}
-        </div>
-        <h5>About</h5>
-        <div className="sm-btn">
-          {/* <img src={send} alt="" /> */}
-        </div>
-      </div>
-      <div className="sectionFirst">
-        <div className="pic">
-          <img
-            src="https://cdn.dribbble.com/users/997665/screenshots/9634478/media/8ad68f67fdbfb09bc054937edecf2d27.png"
-            alt=""
-          />
-        </div>
-        <h3>Alara Frank</h3>
-        <p>
-          Massa id neque aliquam vestibulum. Nibh praesent tristique magna sit.
-          Auctor eu augue ut lectus arcu bibendum at varius. Nam aliquam sem et
-          tortor consequat id. Nunc mi ipsum faucibus vitae aliquet nec. Eu
-          consequat ac felis donec et. Vivamus arcu felis bibendum ut tristique.
-          Egestas diam in arcu cursus euismod quis viverra nibh. Donec ac odio
-          tempor orci dapibus ultrices in iaculis. Enim eu turpis egestas
-          pretium.
-        </p>
-      </div>
-      <div className="sectionSecond">
-        <div className="inner">
-          <h3 className="fadeIn">The talk of what makes a champion.</h3>
-          <p className="fadeIn">
-            Massa id neque aliquam vestibulum. Nibh praesent tristique magna
-            sit. Auctor eu augue ut lectus arcu bibendum at varius. Nam aliquam
-            sem et tortor consequat id. Nunc mi ipsum faucibus vitae aliquet
-            nec. Eu consequat ac felis donec et. Vivamus arcu felis bibendum ut
-            tristique. Egestas diam in arcu cursus euismod quis viverra nibh.
-            Donec ac odio tempor orci dapibus ultrices in iaculis. Enim eu
-            turpis egestas pretium. Tortor vitae purus faucibus ornare
-            suspendisse sed nisi lacus sed. Eget nulla facilisi etiam dignissim
-            diam quis enim lobortis. Enim sit amet venenatis urna cursus eget.
-            Tellus id interdum velit laoreet id. Ac odio tempor orci dapibus. Et
-            ultrices neque ornare aenean euismod elementum nisi. Dolor morbi non
-            arcu risus quis. Lectus sit amet est placerat in egestas erat
-            imperdiet. Cum sociis natoque penatibus et magnis dis.
-          </p>
-          <div className="btn-row fadeIn">
-            <a href="/">Click here bruh</a>
+      <div className="welcome">
+        <div className="welcome__container">
+          <div className="welcome__container__sectionFirst">
+            <h3 className="welcome__container__sectionFirst--title">PORTFOLIO </h3>
+            {/* Parallax made by me here */}
+          </div>
+          <div ref={sectionRef} className="welcome__container__sectionSecond">
+            <div className="inner">
+              <h3 className="fadeIn section2__title">It's time to discover my den!</h3>
+              <p className="fadeIn section2__text">
+                ~ Have a pleasant trip ~
+              </p>
+              <p className="fadeIn allrightsreserved">
+                ©2020 Caroline Liard - All rights reserved
+              </p>
+            </div>
           </div>
         </div>
       </div>
