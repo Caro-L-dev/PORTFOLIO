80887b82c8524b85127f477cfa49d6e374a8d27f
Caro-L-dev <liardcar@gmail.com>
2020-12-01 14:38:04 +0100

    :art: layout contact page

--- src/components/07_Contact/index.js
+++ src/components/07_Contact/index.js
@@ -6,11 +6,37 @@ import React from 'react';
 import './contact.scss';
 
 // == Composant
-const Contact = () => (
-  <div className="contact">
-    <p>Contact</p>
-  </div>
-);
+function Contact() {
+  return (
+    <>
+      <div className="contact">
+        <div className="contact__container">
+          <div className="contact__container--title">
+            <h1>Contact</h1>
+          </div>
+          <div className="contact__container__description">
+            <div
+              className="contact__container__description--img"
+              src=""
+              alt="contact image"
+            />
+            <div className="contact__container__description--emailPhone">
+              <p className="contact__container__description--email">
+                liardcar@gmail.com
+              </p>
+              <p className="contact__container__description--phone">
+                +33 620167545
+              </p>
+              <div className="contact__container__description--logos">
+                Logos ici
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
 
 // == Export
 export default Contact;
