039bc748ffa6611bc8eec2a576b980f9587aa5d6
Caro-L-dev <liardcar@gmail.com>
2020-12-08 20:08:36 +0100

    :hammer: update layout what-i-do page - improve scss 02

--- src/components/03_WhatIDo/index.js
+++ src/components/03_WhatIDo/index.js
@@ -1,7 +1,8 @@
 // == Npm import
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import img1 from '../03_WhatIDo/img1.jpg';
+import img2 from '../03_WhatIDo/speed-mating.jpg';
 
 // == Import
 import './whatido.scss';
@@ -11,40 +12,84 @@ function WhatIDo() {
   return (
     <>
       <div className="whatIDo">
-        <ul>
 
-          <li className="card">
-            <a className="img">
-              <FontAwesomeIcon className="dev--icon" icon={['fab', 'dev']} size="10x" />
-            </a>
-            <article className="card-body">
-              <header>
-                <span className="pre-heading">Web Development</span>
-                <div className="title">
-                  <h3>Opening a door to the future.</h3>
-                </div>
-                <p className="meta">
-                  <span className="text">
-                    Quanta autem vis amicitiae sit,
-                    ex hoc intellegi maxime potest,
-                    quod ex infinita societate generis humani,
-                    Quanta autem vis amicitiae sit,
-                    ex hoc intellegi maxime potest,
-                    quod ex infinita societate generis humani.
-                  </span>
-                </p>
-              </header>
-              <div className="button">
-                <Link to="">
-                  <button type="button" className="whatIDo__button Web">
-                    See More
-                  </button>
-                </Link>
-              </div>
-            </article>
+        <ul>
+          <li>
+            <h1 className="wid__title">
+              What I Do
+            </h1>
+            <h2 className="wid__undertitle">
+              Let me show you what kind of things I can do.
+            </h2>
           </li>
 
+          <div className="wid__card--container">
+            <li className="wid__card">
+              <img className="wid__img" src={img1} alt="dev img" />
+
+              <article className="card-body">
+                <header>
+                  <span className="pre-heading">Web Development</span>
+                  <div className="title">
+                    <h3>Opening a door to the future.</h3>
+                  </div>
+                  <p className="meta">
+                    <span className="text">
+                      Quanta autem vis amicitiae sit,
+                      ex hoc intellegi maxime potest,
+                      quod ex infinita societate generis humani,
+                      Quanta autem vis amicitiae sit,
+                      ex hoc intellegi maxime potest,
+                      quod ex infinita societate generis humani.
+                    </span>
+
+                    <div className="button">
+                      <Link to="">
+                        <button type="button" className="whatIDo__button Web">
+                          See More
+                        </button>
+                      </Link>
+                    </div>
+                  </p>
+
+                </header>
+              </article>
+            </li>
+
+            <li className="wid__card">
+              <img className="wid__img" src={img2} alt="art img" />
+              <article className="card-body">
+                <header>
+                  <span className="pre-heading">Art // Animation</span>
+                  <div className="title">
+                    <h3>Creative World.</h3>
+                  </div>
+                  <p className="meta">
+                    <span className="text">
+                      Quanta autem vis amicitiae sit,
+                      ex hoc intellegi maxime potest,
+                      quod ex infinita societate generis humani,
+                      Quanta autem vis amicitiae sit,
+                      ex hoc intellegi maxime potest,
+                      quod ex infinita societate generis humani.
+                    </span>
+
+                    <div className="button">
+                      <Link to="">
+                        <button type="button" className="whatIDo__button Web">
+                          See More
+                        </button>
+                      </Link>
+                    </div>
+                  </p>
+
+                </header>
+
+              </article>
+            </li>
+          </div>
         </ul>
+
       </div>
     </>
   );
