d66df3ae046f00d877eed1431db708b68ae22c0e
Caro-L-dev <liardcar@gmail.com>
2020-12-09 16:43:23 +0100

    :art: layout about page - jsx

--- src/components/06_About/index.js
+++ src/components/06_About/index.js
@@ -1,16 +1,78 @@
 // == Npm import
 import React from 'react';
-// import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 // == Import
 import './about.scss';
 
 // == Composant
-const About = () => (
-  <div className="about">
-    <p>About</p>
-  </div>
-);
+function About() {
+  return (
+    <>
+      <div className="about">
+        <h1 className="about__title">
+          About
+        </h1>
+        <h2 className="about__undertitle">
+          Know everything about my
+          experiences, and my hobbies.
+        </h2>
+
+        <main className="about__container">
+
+          <section className="about__container--experiences">
+            <h1 className="about__container--experiences-title">
+              Experiences // Objectives
+            </h1>
+            <p className="about__container--experiences-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit, sed do eiusmod tempor incididunt ut labore
+              et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat.
+            </p>
+            <p className="about__container--experiences-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit, sed do eiusmod tempor incididunt ut labore
+              et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat.
+            </p>
+            <p className="about__container--experiences-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit, sed do eiusmod tempor incididunt ut labore
+              et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat.
+            </p>
+          </section>
+
+          <section className="about__container--hobbies">
+            <h1 className="about__container--hobbies-title">
+              Hobbies
+            </h1>
+            <div className="about__container--hobbies-allLogos">
+              Logos fontAwesome here
+            </div>
+          </section>
+
+          <section className="about__container--avatar">
+            <a href="#" alt="Avatar draw by me"> draw here </a>
+          </section>
+
+          <div className="about__container--CV">
+            <Link to="/">
+              <button type="button" className="about__container--CV-button">
+                Download CV
+              </button>
+            </Link>
+          </div>
+
+        </main>
+      </div>
+    </>
+  );
+}
 
 // == Export
 export default About;
