9292245265565877ecfff52b45fc1b163f79f8b5
Caro-L-dev <liardcar@gmail.com>
2020-12-11 18:54:49 +0100

    :hammer: layout about page - zoning scss

--- src/components/06_About/index.js
+++ src/components/06_About/index.js
@@ -10,65 +10,67 @@ function About() {
   return (
     <>
       <div className="about">
-        <h1 className="about__title">
-          About
-        </h1>
-        <h2 className="about__undertitle">
-          Know everything about my
-          experiences, and my hobbies.
-        </h2>
+        <div className="about__container-all">
+          <h1 className="about__title">
+            About
+          </h1>
+          <h2 className="about__undertitle">
+            Know everything about my
+            experiences, and my hobbies.
+          </h2>
 
-        <main className="about__container">
+          <main className="about__container">
 
-          <section className="about__container--experiences">
-            <h1 className="about__container--experiences-title">
-              Experiences // Objectives
-            </h1>
-            <p className="about__container--experiences-text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing
-              elit, sed do eiusmod tempor incididunt ut labore
-              et dolore magna aliqua. Ut enim ad minim veniam,
-              quis nostrud exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat.
-            </p>
-            <p className="about__container--experiences-text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing
-              elit, sed do eiusmod tempor incididunt ut labore
-              et dolore magna aliqua. Ut enim ad minim veniam,
-              quis nostrud exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat.
-            </p>
-            <p className="about__container--experiences-text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing
-              elit, sed do eiusmod tempor incididunt ut labore
-              et dolore magna aliqua. Ut enim ad minim veniam,
-              quis nostrud exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat.
-            </p>
-          </section>
+            <section className="about__container--experiences">
+              <h1 className="about__container--experiences-title">
+                Experiences // Objectives
+              </h1>
+              <p className="about__container--experiences-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing
+                elit, sed do eiusmod tempor incididunt ut labore
+                et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat.
+              </p>
+              <p className="about__container--experiences-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing
+                elit, sed do eiusmod tempor incididunt ut labore
+                et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat.
+              </p>
+              <p className="about__container--experiences-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing
+                elit, sed do eiusmod tempor incididunt ut labore
+                et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat.
+              </p>
+            </section>
 
-          <section className="about__container--hobbies">
-            <h1 className="about__container--hobbies-title">
-              Hobbies
-            </h1>
-            <div className="about__container--hobbies-allLogos">
-              Logos fontAwesome here
-            </div>
-          </section>
+            <section className="about__container--hobbies">
+              <h1 className="about__container--hobbies-title">
+                Hobbies
+              </h1>
+              <div className="about__container--hobbies-allLogos">
+                Logos fontAwesome here
+              </div>
+            </section>
 
-          <section className="about__container--avatar">
-            <a href="#" alt="Avatar draw by me"> draw here </a>
-          </section>
+            <section className="about__container--avatar">
+              <a href="#" alt="Avatar draw by me"> draw here </a>
 
-          <div className="about__container--CV">
-            <Link to="/">
-              <button type="button" className="about__container--CV-button">
-                Download CV
-              </button>
-            </Link>
-          </div>
+              <div className="about__container--CV">
+                <Link to="/">
+                  <button type="button" className="about__container--CV-button">
+                    Download CV
+                  </button>
+                </Link>
+              </div>
+            </section>
 
-        </main>
+          </main>
+        </div>
       </div>
     </>
   );
