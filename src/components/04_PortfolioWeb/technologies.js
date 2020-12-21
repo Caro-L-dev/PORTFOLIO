a498f9d4c57b7c756766995e57a8f512cabd1a9a
Caro-L-dev <liardcar@gmail.com>
2020-12-21 19:50:55 +0100

    :hammer: portfolio wev development page - update style icons

--- src/components/04_PortfolioWeb/technologies.js
+++ src/components/04_PortfolioWeb/technologies.js
@@ -18,89 +18,89 @@ function Technologies() {
         <ul className="pw__technologies--logos">
 
           <li className="pw__logo">
-            <FontAwesomeIcon icon={['fab', 'html5']} size="3x" color="#CE5634" className="icon" />
+            <FontAwesomeIcon icon={['fab', 'html5']} size="6x" color="#CE5634" className="icon" />
             <p>HTML5</p>
           </li>
 
           <li className="pw__logo">
-            <FontAwesomeIcon icon={['fab', 'css3-alt']} size="3x" color="#379AD5" className="icon" />
+            <FontAwesomeIcon icon={['fab', 'css3-alt']} size="6x" color="#379AD5" className="icon" />
             <p>CSS3</p>
           </li>
 
           <li className="pw__logo">
-            <FontAwesomeIcon icon={['fab', 'sass']} size="3x" color="#CE679A" className="icon" />
+            <FontAwesomeIcon icon={['fab', 'sass']} size="6x" color="#CE679A" className="icon" />
             <p>Sass</p>
           </li>
 
           <li className="pw__logo">
-            <FontAwesomeIcon icon={['fab', 'bootstrap']} size="3x" color="#563D7C" className="icon" />
+            <FontAwesomeIcon icon={['fab', 'bootstrap']} size="6x" color="#563D7C" className="icon" />
             <p>Bootstrap</p>
           </li>
 
           <li className="pw__logo">
-            <FontAwesomeIcon icon={['fab', 'js-square']} size="3x" color="#F7DF1E" className="icon" />
+            <FontAwesomeIcon icon={['fab', 'js-square']} size="6x" color="#F7DF1E" className="icon" />
             <p>JavaScript</p>
           </li>
 
           <li className="pw__logo">
-            <FontAwesomeIcon icon={['fab', 'node']} size="3x" color="#71B352" className="icon" />
+            <FontAwesomeIcon icon={['fab', 'node']} size="6x" color="#71B352" className="icon" />
             <p>Node.js</p>
           </li>
 
           <li className="pw__logo">
-            <FontAwesomeIcon icon={['fab', 'react']} size="3x" color="#4AD5FF" className="icon" />
+            <FontAwesomeIcon icon={['fab', 'react']} size="6x" color="#4AD5FF" className="icon" />
             <p>React, Redux</p>
           </li>
 
           <li className="pw__logo--span pw__logo icon">||</li>
 
           <li className="pw__logo">
-            <FontAwesomeIcon icon={['fab', 'git']} size="3x" className="icon" />
+            <FontAwesomeIcon icon={['fab', 'git']} size="6x" className="icon" />
             <p>Git</p>
           </li>
 
           <li className="pw__logo">
-            <FontAwesomeIcon icon={['fab', 'github']} size="3x" className="icon" />
+            <FontAwesomeIcon icon={['fab', 'github']} size="6x" className="icon" />
             <p>Github</p>
           </li>
 
           <li className="pw__logo">
-            <FontAwesomeIcon icon={['fab', 'slack']} size="3x" className="icon" />
+            <FontAwesomeIcon icon={['fab', 'slack']} size="6x" className="icon" />
             <p>Slack</p>
           </li>
 
           <li className="pw__logo">
-            <FontAwesomeIcon icon={['fab', 'discord']} size="3x" color="#6F88DF" className="icon" />
+            <FontAwesomeIcon icon={['fab', 'discord']} size="6x" color="#6F88DF" className="icon" />
             <p>Discord</p>
           </li>
 
           <li className="pw__logo">
-            <FontAwesomeIcon icon={['fab', 'trello']} size="3x" color="#008EE1" className="icon" />
+            <FontAwesomeIcon icon={['fab', 'trello']} size="6x" color="#008EE1" className="icon" />
             <p>Trello</p>
           </li>
 
           <li className="pw__logo">
-            <FontAwesomeIcon icon={['fab', 'yarn']} size="3x" color="#2C8EBB" className="icon" />
+            <FontAwesomeIcon icon={['fab', 'yarn']} size="6x" color="#2C8EBB" className="icon" />
             <p>Yarn</p>
           </li>
 
           <li className="pw__logo">
-            <FontAwesomeIcon icon={['fab', 'npm']} size="3x" color="#CB3837" className="icon" />
+            <FontAwesomeIcon icon={['fab', 'npm']} size="6x" color="#CB3837" className="icon" />
             <p>Npm</p>
           </li>
 
           <li className="pw__logo">
-            <FontAwesomeIcon icon={['fab', 'linux']} size="3x" className="icon" />
+            <FontAwesomeIcon icon={['fab', 'linux']} size="6x" className="icon" />
             <p>Linux</p>
           </li>
 
           <li className="pw__logo">
-            <FontAwesomeIcon icon={['fa', 'code']} size="3x" color="#2CA4F1" className="icon" />
+            <FontAwesomeIcon icon={['fa', 'code']} size="6x" color="#2CA4F1" className="icon" />
             <p>VS Code</p>
           </li>
 
           <li className="pw__logo">
-            <FontAwesomeIcon icon={['fab', 'font-awesome-flag']} size="3x" color="#1661A5" className="icon" />
+            <FontAwesomeIcon icon={['fab', 'font-awesome-flag']} size="6x" color="#1661A5" className="icon" />
             <p>Font Awesome</p>
           </li>
 
