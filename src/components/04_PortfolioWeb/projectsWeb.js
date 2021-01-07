55826fb500d71f8f865780e39b9e7be502254554
Caro-L-dev <liardcar@gmail.com>
2021-01-07 13:09:04 +0100

    :hammer: portfolio web dev page - add carousel on projects

--- src/components/04_PortfolioWeb/projectsWeb.js
+++ src/components/04_PortfolioWeb/projectsWeb.js
@@ -1,11 +1,13 @@
 // == Npm import
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import Carousel, { slidesToShowPlugin } from '@brainhubeu/react-carousel';
 import HeroImg from './Hero_miniature.png';
 import TrexrunImg from './Trexrun.png';
 import PokedexImg from './Pokedex.jpg';
 
-// == Import
+// == Import scss
+import '@brainhubeu/react-carousel/lib/style.css';
 import './portfolioWeb.scss';
 
 // == Composant
@@ -15,45 +17,54 @@ function ProjectsWeb() {
       <section className="pw__projects">
         <h2 className="pw__title">Portfolio</h2>
 
-        <FontAwesomeIcon icon={['fa', 'chevron-left']} size="2x" />
-
-        <div className="card">
-          <a href="http://34.207.247.234:5000/#/">
-            <img
-              className="pw__img"
-              src={HeroImg}
-              alt="Miniature Hero Website"
-            />
-          </a>
-        </div>
-
-        <div className="card">
-          <a href="https://github.com/Caro-L-dev/T-rex-run">
-            <img
-              className="pw__img"
-              src={TrexrunImg}
-              alt="Miniature mini game Js T-rex run"
-            />
-          </a>
-        </div>
-
-        <div className="card">
-          <img
-            className="pw__img"
-            src={PokedexImg}
-            alt="Miniature pokedex"
-          />
-        </div>
-
-        <div className="card">
-          <div className="pw__img">Miniature 03</div>
-          <div className="info">
-            <h3>Title</h3>
-            <p>Text</p>
+        <Carousel
+          plugins={[
+            'infinite',
+            'arrows',
+            {
+              resolve: slidesToShowPlugin,
+              options: {
+                numberOfSlides: 4,
+              },
+            },
+          ]}
+        >
+          <div className="card">
+            <a href="http://34.207.247.234:5000/#/">
+              <img
+                className="pw__img"
+                src={HeroImg}
+                alt="Miniature Hero Website"
+              />
+            </a>
           </div>
-        </div>
 
-        <FontAwesomeIcon icon={['fa', 'chevron-right']} size="2x" />
+          <div className="card">
+            <a href="https://github.com/Caro-L-dev/T-rex-run">
+              <img
+                className="pw__img"
+                src={TrexrunImg}
+                alt="Miniature mini game Js T-rex run"
+              />
+            </a>
+          </div>
+
+          <div className="card">
+            <img
+              className="pw__img"
+              src={PokedexImg}
+              alt="Miniature pokedex"
+            />
+          </div>
+
+          <div className="card">
+            <div className="pw__img">Miniature 03</div>
+            <div className="info">
+              <h3>Title</h3>
+              <p>Text</p>
+            </div>
+          </div>
+        </Carousel>
       </section>
 
     </>
