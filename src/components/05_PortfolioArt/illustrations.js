b6569b5581c90449ca619a77d2a4661ebe11026e
Caro-L-dev <liardcar@gmail.com>
2021-01-29 17:43:58 +0100

    portfolio art page - import images

--- src/components/05_PortfolioArt/illustrations.js
+++ src/components/05_PortfolioArt/illustrations.js
@@ -1,6 +1,7 @@
 // == Npm import
 import React from 'react';
 import './illustrations.scss';
+import './portfolioArt.scss';
 
 // == Composant
 function Illustrations() {
@@ -8,15 +9,15 @@ function Illustrations() {
     <>
       <section className="pa__illustrations">
         <div className="pa__illustrations--container">
-          <div className="cell cell-1">1.</div>
-          <div className="cell cell-2">2.</div>
-          <div className="cell cell-3">3.</div>
-          <div className="cell cell-4">4.</div>
-          <div className="cell cell-5">5.</div>
-          <div className="cell cell-6">6.</div>
-          <div className="cell cell-7">7.</div>
-          <div className="cell cell-8">8.</div>
-          <div className="cell cell-9">9.</div>
+          <div className="cell cell-1"></div>
+          <div className="cell cell-2"></div>
+          <div className="cell cell-3"></div>
+          <div className="cell cell-4"></div>
+          <div className="cell cell-5"></div>
+          <div className="cell cell-6"></div>
+          <div className="cell cell-7"></div>
+          <div className="cell cell-8"></div>
+          <div className="cell cell-9"></div>
         </div>
       </section>
     </>
